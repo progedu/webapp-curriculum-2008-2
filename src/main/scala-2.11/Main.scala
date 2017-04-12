521d092aaa8494c699259d96a5af28d9f2376abe
murosan <masashi_16n1200013@nnn.ed.jp>
2017-04-12 16:31:38 +0900

    挑戦の中級

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,3 +1,15 @@
 object Main {
 
+  implicit class RichList[T](val list: List[T]) {
+    def compress: List[T] = {
+      def compressRecursive(li: List[T], pressed: List[T]): List[T] = li match {
+        case Nil => pressed
+        case x :: xs =>
+          if (pressed contains x) compressRecursive(xs, pressed)
+          else compressRecursive(xs, pressed ++ List(x))
+      }
+      compressRecursive(list, List())
+    }
+  }
+
 }
