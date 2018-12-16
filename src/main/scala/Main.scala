4d5411ccaa5f91e33a8b037a5159268ec988b0ca
Masahiro <masahiro.infinite77@gmail.com>
2018-12-17 05:13:00 +0900

    Stringに長さを制限文字数内に収める機能を追加

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,12 @@
 object Main {
+  implicit class CutStrings(val src: String) {
+    def cut(n: Int): String = {
+      def cutRec(index: Int, part: String): String = {
+        if (index == n || index > src.size -1) part
+        else cutRec(index + 1, part + src(index))
+      }
+      cutRec(0, "")
+    }
+  }
 
 }
