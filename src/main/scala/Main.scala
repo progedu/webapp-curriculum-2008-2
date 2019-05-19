f52f080d2dcc44224b03f9cb73e7d80a027e122a
syo <M.youta.kwg@gmail.com>
2019-05-19 09:07:42 +0900

    指定の数だけ草を生やせるgrassメソッドをStringへ追加

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,10 @@
 object Main {
-
+  implicit class RichString(src: String) {
+    // 指定の数だけ草を生やせるため、正確に気持ちを表現できる
+    // 単芝チェックを行い、単芝うぜぇと言われるのを防止する
+    def grass(num: Int): String = {
+      require(num > 1 || num == 0, "単芝またはマイナスの草は禁止されています")
+      src + "ｗ" * num
+    }
+  }
 }
