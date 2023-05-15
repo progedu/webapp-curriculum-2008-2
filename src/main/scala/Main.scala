39c103de1a312ac8a22e00cfa909aad5bf0b3f60
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-15 14:38:16 +0900

    ExtendNumsの定義

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
 object Main {
 
 }
+
+implicit class ExtendNums[T >: AnyVal](val value: T) {
+  def sqrt: T = value match {
+    case x: Double => Math.sqrt(x)
+    case x: Int => Math.sqrt(x.toDouble).toInt
+    case x: Long => Math.sqrt(x.toDouble).toLong
+  }
+}
